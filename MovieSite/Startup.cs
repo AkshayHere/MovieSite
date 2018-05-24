ffad27193ad83c4e8a926ddb92a95d1b54b62480
AkshayHere <akshaymohanrocks@gmail.com>
2018-05-25 00:37:37 +0800

    Add project files.

---
+++ MovieSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieSite.Startup))]
+namespace MovieSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
